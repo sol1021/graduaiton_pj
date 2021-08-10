1e27d389ba9fcea15179fd2fa6f780d517e58ab7
sol1021 <62051781+sol1021@users.noreply.github.com>
2021-08-10 16:42:24 +0900

    08-10

---
+++ src/templates/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Cam=() => {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      <img id="video"   src="http://localhost:5000/video_viewer"/>
+    </div>
+  );
+};
+
+export default Cam;
